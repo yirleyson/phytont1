73aea87bbf3c21bf4e29e12489ccc58a8cb897eb
Yino Ramirez <62155715+yirleyson@users.noreply.github.com>
2020-08-16 17:56:44 -0500

    Taller 2
    Condicionales

---
+++ condicionales.py
@@ -0,0 +1,59 @@
+# #1
+#pint=float(input("Ingrese el interes "))
+#cap=float(input("Ingrese el capital que desea invertir"))
+
+#int=cap*pint
+
+#if int>7000:
+#    capf=cap+int
+
+#print(F"El capital final es: {capf}")
+
+
+
+# #2
+
+#calif1=int(input("Ingrese su primera calificación"))
+#calif2=int(input("Ingrese su segunda calificación"))
+#calif3=int(input("Ingrese su tercera calificación"))
+#prom=(calif1+calif2+calif3)/3
+
+#if prom>=70:
+#    print("Alumno aprobado")
+#else:
+#    print("Alumno reprobado")
+
+# #3
+#compra=float(input("Ingrese el precio de la compra"))
+
+#if compra>1000:
+#    des=compra*0.20
+#else:
+#    desc=0
+
+#totpag=compra-des
+#print(f"El total a pagar es de: {totpag}")
+
+# #4
+#ht=int(input("Ingrese el número de horas trabajadas"))
+
+#if ht>40:
+#    he=ht-40
+#    ss=he*20+40*16
+#else:
+#    ss=ht*16
+
+#print(f"El salario es de: {ss}")
+
+
+# #6
+#num1=int(input("Ingrese un número"))
+#num2=int(input("Ingrese un número"))
+
+#if num1<num2:
+#    print(f"{num1}  {num2}")
+#else:
+#    print(f"{num2}  {num1}")
+
+
+
