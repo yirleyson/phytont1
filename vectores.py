68037a67605c44328172c76bc2eb039dc7ef2ff5
Yino Ramirez <62155715+yirleyson@users.noreply.github.com>
2020-08-20 04:39:12 -0500

    Listas
    Taller de listas

---
+++ vectores.py
@@ -0,0 +1,36 @@
+#Eliminar elementos repetidos
+#lista=[1,2,3,4,"Hola",2,2]
+#conjunto=set(lista)
+#print(conjunto)
+
+
+# ejercicio #2
+
+lista1=["hola","que","como","quien","donde"]
+print(lista1)
+
+lista2=["cuando","yino","hola","por que","como"]
+print(lista2)
+
+lis1=["que","quien","donde"]
+print(lis1)
+lis2=["cuando","yino","por que"]
+print(lis2)
+
+lis3=lista1+lista2
+print(lis3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
